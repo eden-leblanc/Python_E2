0fed3cbe10cd346cbec507eaab18ab4e06d84470
eden <eden.leblanc@edu.esiee.fr>
2025-11-06 09:38:54 +0100

    Exercice 1 nombres premiers

---
+++ Fonctions et module/main.py
@@ -0,0 +1,30 @@
+from math import sqrt
+
+#### Fonction secondaire
+
+
+def isprime(n):
+    res = True
+    for i in range (2, n**0.5):
+        if n % i == 0:
+            res = False
+            break
+    return res
+    pass
+
+#### Fonction principale
+
+
+def main():
+
+    # vos appels à la fonction secondaire ici
+
+    for n in range(100):
+        if isprime(n):
+            print(n, end=", ")
+
+    print()
+
+
+if __name__ == "__main__":
+    main()
